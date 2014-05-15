c9892b7dd7e3c7184379ea4d3d25aeab99cd998e
Kevin Krieger <kevinjkrieger@gmail.com>
2014-05-14 22:17:01 -0600

    Ignore axf, object, and debug files

--- lpcxpresso/lpc_chip_11cxx_lib/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.d: ../src/ring_buffer.c \
- /home/kevin/lpcxpresso/lpc_chip_11cxx_lib/inc/ring_buffer.h \
- /home/kevin/lpcxpresso/lpc_chip_11cxx_lib/inc/lpc_types.h
-
-/home/kevin/lpcxpresso/lpc_chip_11cxx_lib/inc/ring_buffer.h:
-
-/home/kevin/lpcxpresso/lpc_chip_11cxx_lib/inc/lpc_types.h:
--- lpcxpresso/obd2/Debug/src/cr_startup_lpc11xx.d
+++
@@ -1,1 +0,0 @@
-src/cr_startup_lpc11xx.d: ../src/cr_startup_lpc11xx.c
